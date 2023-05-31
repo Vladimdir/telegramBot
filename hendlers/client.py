75428e42be98a6b5887eee33b6a5157f4d8d639b
dom <feofanzatv@yandex.ru>
2023-05-31 20:44:58 +0300

    Кнопки meters

--- hendlers/client.py
+++ hendlers/client.py
@@ -2,30 +2,38 @@ from aiogram import types
 from aiogram.types import CallbackQuery
 from aiogram.utils.markdown import text
 from keyboards import client_kd
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
 
 from create_bot import dp, bot, Dispatcher
 
 
 # @dp.message_handler(commands=['start'])
-async def info(message: types.Message):
+async def info(message: types.Message) -> None:
     await message.answer_photo(photo=open('img/dom.jpg', 'rb'), caption=f'Hello, {message.from_user.first_name}!',
                                reply_markup=client_kd.start_menu)
 
 
 # @dp.callback_query_handler(text='create_account')
-async def registration(call: types.CallbackQuery) -> None:
-    await call.message.answer_photo(photo=open('img/dom.jpg', 'rb'), caption='<b>ЗАРЕЄСТРУВАТИСЯ</b> ↘️:',
+async def registration(call: types.CallbackQuery, state) -> None:
+    msg = await call.message.answer_photo(photo=open('img/dom.jpg', 'rb'), caption='<b>ЗАРЕЄСТРУВАТИСЯ</b> ↘️:',
                                     reply_markup=client_kd.btn_reg2, parse_mode='HTML')
+    async with state.proxy() as data:
+        data['message_id'] = msg.message_id
     await call.message.delete()
 
 
-# @dp.message_handler(content_types=['contact'])
-async def reg_phone(message: types.Message):
+# @dp.message_handler(content_types=['contact']) # message.contact.phone_number[-10:]
+async def reg_phone(message: types.Message, state) -> None:
     if message.contact is not None:
-        print(message.contact.phone_number[-10:])
+        print("Номер телефона")
     await message.delete()
-    await bot.send_message(message.from_user.id, f'Хвилинку, перевіряю номер телефону... 🧐',
-                           reply_markup=types.ReplyKeyboardRemove())
+    async with state.proxy() as data:
+        message_id = data['message_id']
+    await bot.delete_message(message.from_user.id, message_id)
+    await message.answer_photo(photo=open('img/dom.jpg', 'rb'), caption=f"<b>ОСОБИСТИЙ КАБІНЕТ</b>\n\nОсобовий рахунок"
+                                                                        f": {260418}\n\nДо сплати: {4502.36}грн",
+                                    reply_markup=client_kd.account, parse_mode='HTML')
 
 
 # @dp.callback_query_handler(text='kod_register')
@@ -41,12 +49,34 @@ async def start(call: types.CallbackQuery) -> None:
     pass
 
 
+# @dp.callback_query_handler(text='submit_meter')
+async def meters(call: types.CallbackQuery, state) -> None:
+    await call.message.answer_photo(photo=open('img/dom.jpg', 'rb'), caption='<b>Виберіть прилад обліку</b> ↘️:',
+                                    reply_markup=client_kd.meter, parse_mode='HTML')
+    await call.message.delete()
+
+
+# @dp.callback_query_handler(text='water')
+async def water(call: types.CallbackQuery, state) -> None:
+
+    await call.message.answer_photo(photo=open('img/meterWater.jpg', 'rb'), caption='<b>Введіть показники лічильника ХОЛОДНОЇ ВОДИ (ціле число)</b> ↘️:',
+                                    reply_markup=client_kd.back_meters, parse_mode='HTML')
+    await call.message.delete()                            
+    
+
+async def meters_water(message: types.Message):
+    await message.answer('Показники прийняті.')
+
+
 def register_handlers_client(disp: Dispatcher):
     disp.register_message_handler(info, commands=['start'])
     disp.register_callback_query_handler(registration, text='create_account')
     disp.register_callback_query_handler(kod_register, text='kod_register')
     disp.register_message_handler(reg_phone, content_types=['contact'])
-    # disp.register_callback_query_handler(start, text='start')
+    disp.register_callback_query_handler(meters, text='submit_meter')
+    disp.register_callback_query_handler(water, text='water')
+    disp.register_message_handler(meters_water)
+
 
 
 
--- keyboards/client_kd.py
+++ keyboards/client_kd.py
@@ -3,20 +3,33 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeybo
 
 start_menu = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton('📞 Контакти УК', url='https://telegra.ph/Kontakti-04-26'),
+        InlineKeyboardButton('📞 Контакти УК', url='https://telegra.ph'),
         InlineKeyboardButton('Розклад руху 🚆', url='https://telegra.ph/rfgrgfefdgdfgfdgfdg-04-14'),
     ],
-    [InlineKeyboardButton('💳 Реквізити для оплати', url='https://telegra.ph/REKV%D0%86ZITI-TOV-UK-Kocyubinske-04-26')],
-    [InlineKeyboardButton('📝 Cтворити особистий кабінет', callback_data='create_account')]
-])
-
-
-btn_reg = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton('🔐 - по коду реєстрації', callback_data='kod_register')],
-    [InlineKeyboardButton('📱 - за номером телефону', callback_data='acc_phone')],
-    [InlineKeyboardButton('Назад', callback_data='start')]
+    [InlineKeyboardButton('💳 Реквізити для оплати', url='https://telegra.ph')],
+    [InlineKeyboardButton('🛠 Cтворити особистий кабінет', callback_data='create_account')]
 ])
 
 btn_reg2 = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Реєстрація по телефону', request_contact=True)]
 ], resize_keyboard=True)
+
+account = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('📞 Контакти УК', url='https://telegra.ph'),
+        InlineKeyboardButton('Розклад руху 🚆', url='https://telegra.ph/rfgrgfefdgdfgfdgfdg-04-14'),
+    ],
+    [InlineKeyboardButton('💳 Реквізити для оплати', url='https://telegra.ph')],
+    [InlineKeyboardButton('📝 Передати показники ', callback_data='submit_meter')]
+])
+
+meter = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton('💧 Холодна вода', callback_data='water')],
+    [InlineKeyboardButton('⚡️ Електроенергія', callback_data='electricity')],
+    [InlineKeyboardButton('🔥 Опалення', callback_data='heating')],
+    [InlineKeyboardButton(text='« Назад', callback_data='5555555')]
+])
+
+back_meters = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='« Назад', callback_data='submit_meter')]
+    ])
